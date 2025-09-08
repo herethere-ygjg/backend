37b1460b85d0db2e98e551fe2dae7c1a2164cf97
songhajang <angpan2004@gmail.com>
2025-09-08 23:50:25 +0900

    feat: spring 프로젝트 기본 패키지 추가

---
+++ src/main/java/com/YGJG/hereThere/HereThereApplication.java
@@ -0,0 +1,13 @@
+package com.YGJG.hereThere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereThereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HereThereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/YGJG/hereThere/HereThereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YGJG.hereThere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HereThereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
